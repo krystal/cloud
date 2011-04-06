55f737cdc05e58e480417bfdd521d63fc06a200f
Adam Cooke <adam@atechmedia.com>
2011-04-06 15:51:10 +0100

    add new deploy stuff

--- lib/atech_cloud/deploy.rb
+++ lib/atech_cloud/deploy.rb
@@ -4,6 +4,11 @@ Capistrano::Configuration.instance(:must_exist).load do
   set :user, `whoami`.chomp
   set :ssh_options, {:forward_agent => true, :port => 22}
   
+  ## Return the deployment path
+  def deploy_to
+    fetch(:deploy_to, nil) || "/opt/apps/#{fetch(:application)}"
+  end
+  
   ## Deployment namespace
   namespace :deploy do
     desc 'Deploy the latest revision of the application'
@@ -19,33 +24,36 @@ Capistrano::Configuration.instance(:must_exist).load do
     end
 
     task :update_code, :roles => :app do
-      path = fetch(:deploy_to)
       ## Create a branch for previous (pre-deployment)
-      run "cd #{path} && git branch -d rollback && git branch rollback"
+      run "cd #{deploy_to} && git branch -d rollback && git branch rollback"
       ## Update remote repository and merge deploy branch into current branch
-      run "cd #{path} && git fetch origin && git reset --hard origin/#{fetch(:branch)}"
+      run "cd #{deploy_to} && git fetch origin && git reset --hard origin/#{fetch(:branch)}"
       finalise
     end
 
     task :finalise, :roles => :app do
       execute = Array.new
-      execute << "cd #{fetch(:deploy_to)}"
+      execute << "cd #{deploy_to}"
       execute << "git submodule init"
       execute << "git submodule sync"
       execute << "git submodule update --recursive"
       run execute.join(' && ')
 
-      run "cd #{fetch(:deploy_to)} && bundle --deployment --quiet"
+      run "cd #{deploy_to} && bundle --deployment --quiet"
       migrate if fetch(:run_migrations, false)
     end
 
     desc 'Setup the repository on the remote server for the first time'
     task :setup, :roles => :app do
-      path = fetch(:deploy_to)
-      run "git clone -n #{fetch(:repository)} #{path} --branch #{fetch(:branch)}"
-      run "cd #{path} && git branch rollback && git checkout -b deploy && git branch -d #{fetch(:branch)}"
-      run "sed 's/socket: \\\/tmp\\\/mysql.sock/host: db-a.cloud.atechmedia.net/' #{path}/config/database.yml.example > #{path}/config/database.yml ; true"
+      run "git clone -n #{fetch(:repository)} #{deploy_to} --branch #{fetch(:branch)}"
+      run "cd #{deploy_to} && git branch rollback && git checkout -b deploy && git branch -d #{fetch(:branch)}"
+      upload_db_config
       update_code
+    end
+    
+    desc 'Upload the database configuration file'
+    task :upload_db_config, :roles => :app do
+      put "[production]\n  adapter: mysql2\n  encoding: utf8\n  reconnect: false\n  database: #{fetch(:application, 'databasename')}\n  pool: 5\n  username: #{fetch(:application, 'dbusernmae')}\n  password: #{ENV['DBPASS'] || 'xxxx'}\n  host: #{fetch(:database_host, 'db-a.cloud.atechmedia.net')}\n", File.join(deploy_to, 'config', 'database.yml')
     end
   end
   
@@ -54,7 +62,7 @@ Capistrano::Configuration.instance(:must_exist).load do
   ## ==================================================================
   desc 'Run database migrations on the remote'
   task :migrate, :roles => :app, :only => {:database_ops => true} do
-    run "cd #{fetch(:deploy_to)} && RAILS_ENV=#{fetch(:environment)} bundle exec rake db:migrate"
+    run "cd #{deploy_to} && RAILS_ENV=#{fetch(:environment)} bundle exec rake db:migrate"
   end
 
   ## ==================================================================
@@ -62,7 +70,7 @@ Capistrano::Configuration.instance(:must_exist).load do
   ## ==================================================================
   desc 'Rollback to the previous deployment'
   task :rollback, :roles => :app do
-    run "cd #{fetch(:deploy_to)} && git reset --hard rollback"
+    run "cd #{deploy_to} && git reset --hard rollback"
     deploy.finalise
     deploy.restart
   end
@@ -101,15 +109,26 @@ Capistrano::Configuration.instance(:must_exist).load do
   ## ==================================================================
   namespace :unicorn do
     task :start, :roles => :app  do
-      run "sudo -u app sh -c \"cd #{fetch(:deploy_to)} && bundle exec unicorn_rails -E #{fetch(:environment)} -c #{fetch(:deploy_to)}/config/unicorn.rb -D\""
+      upload_config
+      run "sudo -u app sh -c \"cd #{deploy_to} && bundle exec unicorn_rails -E #{fetch(:environment)} -c #{deploy_to}/config/unicorn.rb -D\""
     end
 
     task :stop, :roles => :app do
-      run "sudo -u app sh -c \"kill `cat #{fetch(:deploy_to)}/tmp/pids/unicorn.pid`\""
+      run "sudo -u app sh -c \"kill `cat #{deploy_to}/tmp/pids/unicorn.pid`\""
     end
 
     task :restart, :roles => :app do
-      run "sudo -u app sh -c \"kill -USR2 `cat #{fetch(:deploy_to)}/tmp/pids/unicorn.pid`\""
+      upload_config
+      run "sudo -u app sh -c \"kill -USR2 `cat #{deploy_to}/tmp/pids/unicorn.pid`\""
+    end
+    
+    task :upload_config, :roles => :app do
+      unless fetch(:skip_unicorn_config, false)
+        template_config = File.read(File.expand_path('../unicorn.rb', __FILE__))
+        template_config.gsub!('$WORKER_PROCESSES', fetch(:unicorn_workers, 4).to_s)
+        template_config.gsub!('$TIMEOUT', fetch(:unicorn_timeout, 30).to_s)
+        put template_config, File.join(deploy_to, 'config', 'unicorn.rb')
+      end
     end
   end
   
---
+++ lib/atech_cloud/unicorn.rb
@@ -0,0 +1,52 @@
+## Directory to use for stuff
+rails_root = File.expand_path('../../', __FILE__)
+FileUtils.mkdir_p(File.join(rails_root, 'tmp', 'sockets'))
+FileUtils.mkdir_p(File.join(rails_root, 'tmp', 'pids'))
+FileUtils.mkdir_p(File.join(rails_root, 'log'))
+
+## Set the number of worker processes which can be spawned
+worker_processes $WORKER_PROCESSES
+
+## Preload the application into master for fast worker spawn
+## times.
+preload_app true
+
+## Restart any workers which haven't responded for 30 seconds.
+timeout $TIMEOUT
+
+## Store the pid file safely away in the pids folder
+logger Logger.new(File.join(rails_root, 'log', 'unicorn.log'))
+pid File.join(rails_root, 'tmp', 'pids', 'unicorn.pid')
+
+## Listen on a unix data socket
+listen File.join(rails_root, 'tmp', 'sockets', 'unicorn.sock')
+
+before_fork do |server, worker|
+  # When sent a USR2, Unicorn will suffix its pidfile with .oldbin and
+  # immediately start loading up a new version of itself (loaded with a new
+  # version of our app). When this new Unicorn is completely loaded
+  # it will begin spawning workers. The first worker spawned will check to
+  # see if an .oldbin pidfile exists. If so, this means we've just booted up
+  # a new Unicorn and need to tell the old one that it can now die. To do so
+  # we send it a QUIT.
+  #
+  # Using this method we get 0 downtime deploys.
+
+  old_pid = File.join(rails_root, 'tmp', 'pids', 'unicorn.pid.oldbin')
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      Process.kill("QUIT", File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+      # someone else did our job for us
+    end
+  end
+end
+
+after_fork do |server, worker|
+  # Unicorn master loads the app then forks off workers - because of the way
+  # Unix forking works, we need to make sure we aren't using any of the parent's
+  # sockets, e.g. db connection
+  ActiveRecord::Base.establish_connection
+  srand
+end
+
